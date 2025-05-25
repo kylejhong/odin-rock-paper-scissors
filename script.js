391542a67adb843798c6a572f73744d80f18b71b
Kyle Jhong <29828796+kylejhong@users.noreply.github.com>
2025-05-24 20:48:14 -0700

    Added Human Choice

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-console.log("Hello World");
+let humanScore = 0;
+let computerScore = 0;
+
+console.log(getComputerChoice());
+console.log(getHumanChoice());
 
 function getComputerChoice() {
     let number = parseInt(Math.random() * 3);
@@ -9,5 +13,20 @@ function getComputerChoice() {
         return "paper";
     } else {
         return "scissors";
+    }
+}
+
+function getHumanChoice() {
+    let userInput = prompt("Please enter either \"rock, paper, or scissors.\"")
+    userInput = userInput.trim();
+    userInput = userInput.toLowerCase();
+    if (userInput === "rock") {
+        return "rock";
+    } else if (userInput === "paper") {
+        return "paper";
+    } else if (userInput === "scissors") {
+        return "scissors";
+    } else {
+        return null;
     }
 }
